434629966d5c5a6ee8bc133afda2d5b96919e24b
ShivamDh <shdh8888@yahoo.ca>
2016-10-20 18:11:38 -0400

    Updated version, less unrelated info

--- bitConverter.c
+++ bitConverter.c
@@ -7,11 +7,25 @@ int main() {
 	scanf ("%d", &number1);
 	
 	printf("The first number in binary form is: ");
+	uint64_t number1_bits[32];
 	for (uint8_t i = 0; i<=31; i++) {
-		uint64_t temp_number1 = number1 >> (31-i);
-		temp_number1 = temp_number1 & 1;
-		printf("%d", temp_number1);
+		number1_bits [i] = number1 >> (31-i);
+		number1_bits[i] = number1_bits[i] & 1;
 		}
-		
+	
+	uint8_t blanks = 0;
+	
+	for (uint8_t j = 0; j < 32; j++) {
+		if (number1_bits[j] == 0) {
+			printf("");
+			blanks++;
+		}
+		else 
+			break;
+	}
+	
+	for (blanks; blanks < 32; blanks++) 
+		printf ("%d", number1_bits[blanks]);
+	
 	return 0;
 }
