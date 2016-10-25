8e6ec3c8d45310d0a07ce0bbaa2b9b3bb5a43443
ShivamDh <shdh8888@yahoo.ca>
2016-10-25 00:22:04 -0400

    A Calculator that expresses operators and operaitons in Bits

--- bitCalculator.c
+++ bitCalculator.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+int bits (int bit_number);
+
 int main () {
 	uint8_t operation = 0; //Initialization of variables and values
-	uint64_t number1 = 0;
-	uint64_t number2 = 0;
-	uint64_t answer = 0;
+	uint32_t number1 = 0;
+	uint32_t number2 = 0;
+	uint32_t answer = 0;
 	printf ("Enter the two numbers you would like to operate on: ");
-	scanf ("%d %d", &number1, &number2);
+	scanf ("%d %d", &number1, &number2); // Take in user input of two numbers to be used
 	
 	printf ("  (1) Addition \n  (2) Subtraction \n  (3) Multiplication \
 	\n Enter your choice:  ");
@@ -25,8 +27,38 @@ int main () {
 		default: printf ("Sorry wrong number entered \n"); return -1;
 		break;
 	}
-	printf("The operation is equal to %d", answer); //Give back answer in decimal form
 	
+	printf(" The first number in binary form is: "); bits(number1); // Print Number 1 in decimal 
+	
+	printf("\n The second number in binary form is: "); bits(number2); // Print Number 2 in binary
+	
+	printf("\n The operation is equal to %d", answer); // Print answer in decimal 
+	
+	printf("\n The answer in binary form is: "); bits(answer); // Print answer in binary again
 	
 	return 0;
+}
+
+int bits (int bit_number) {
+	uint32_t number_bits[64]; // An array to store value of each bit
+	for (uint8_t i = 0; i < 64; i++) {
+		number_bits [i] = bit_number>> (63-i);
+		number_bits[i] = number_bits[i] & 1; // Storing all the bits within the array created
+		}
+	
+	uint8_t blanks = 0;
+	
+	for (uint8_t j = 0; j < 32; j++) { //Weeding out the initial zeroes seen in binary code due to 64-bit variable
+		if (number_bits[j] == 0) {
+			printf(""); // Do nothing if the inital bits are '0', aka. do not print the '0'
+			blanks++; // Keep track of how many initial '0' there are
+		}
+		else 
+			break; 
+	}
+	
+	for (blanks; blanks < 32; blanks++) // Start printing from the first non-zero digit
+		printf ("%d", number_bits[blanks]); //Printing the relevant binary code 
+
+	
 }
