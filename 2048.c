adc726fd796d84cb9c2e95325cbf6deeb89e5135
ShivamDh <shdh8888@yahoo.ca>
2016-11-08 00:20:00 -0500

    Creating a print function for the array

--- 2048.c
+++ 2048.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+void printArea (int values[4][4]);
+
 int main () {
-	int box[4][4] = { {0, 1, 2, 3}, 
-					  {5, 6, 7, 8}, 
-					  {8, 9, 10, 11},
-					  {12, 13, 14, 15}};
-		
+	int box[4][4] = { {0, 10, 69, 0}, 
+					  {10, 1, 1, 1}, 
+					  {695, 1024, 1, 1},
+					  {5102, 1024, 2048, 8}};
+	
+	printArea(box);
+	
+}
+
+void printArea (int values[4][4]) {
+	for (int a = 0; a < 65; a++) {
+			printf ("-");
+	}
+	
+	printf ("\n");
+	
 	for (int i = 0; i<4; i++) {
 		for (int j = 0; j<4; j++) {
-			if (box[i][j] == 0) 
-				printf ("| \t");
-			else
-				printf ("| %d \t", box[i][j]);
+				printf ("|\t%d\t", values[i][j]);	
+		}
+		}
+		printf ("|");
+		printf ("\n");
+		for (int k = 0; k < 65; k++) {
+			printf ("-");
 		}
 		printf ("\n");
 	}
 }
-
