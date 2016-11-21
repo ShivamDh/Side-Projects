ea18507aa43ccc1a2809e6e5f6a77672e381d6f7
ShivamDh <shdh8888@yahoo.ca>
2016-11-21 02:12:50 -0500

    Moving forward with initial stages of the project

--- molarMassCalc.c
+++ molarMassCalc.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float calcMM (char* substance, int length);
+
 int main (int argc,char* argv[]) {
 	if(argc == 1) { //no command line argumnet for compound
 		char compound1[25]; //reasonable to assume no compound over 25 letters will be used
 		printf ("Please enter a compound who's molar mass you would like to calculate: ");
 		int i = 0;
 		compound1[i] = getchar();
-		while (compound1[i] != '\0' && compound1[i] != '\n' && i < 25) {
-			i++;
+		while (compound1[i] != '\0' && compound1[i] != '\n' && i < 25) { //store variables until input ends
+			if (!(compound1[i] == 40 || compound1[i] == 41 || (compound1[i] > 47 && compound1[i] < 58) || 
+			(compound1[i] > 64 && compound1[i] < 91) || (compound1[i] > 96 && compound1[i] < 123))) {
+				printf ("Incorrect characters used to represent a compound"); //checked for invalid characters
+				return -1;
+			}
+			i++; //i = number of characters, last compound[i] stores '\n'
+			compound1[i] = getchar(); //update for next while loop
 		}
-		printf ("%c", compound1[0]);
+		printf ("%d", i);
+		
+		float theMM = calcMM(compound1, i);
+		if (theMM < 0)
+			printf ("An error has occurred, please check the characters used for the formulae");
+		else 
+			printf ("\nThe molar mass is: %.3f", theMM);
 	}
 	
 	return 0;
+}
+
+float calcMM (char* substance, int length) {
+	float M = 0;
+		
+	return M;
 }
