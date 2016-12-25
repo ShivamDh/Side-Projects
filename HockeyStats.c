487547d8590ea0e7ef7fc352d4089ddfab54db91
ShivamDh <shdh8888@yahoo.ca>
2016-12-24 23:00:26 -0500

    Fixed bugs in readFile function, starting on next portion of program

--- HockeyStats.c
+++ HockeyStats.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-struct Players {
+typedef struct Players {
+	//inputted basic statistics
 	char name[50]; //first and/or last name of person
 	unsigned short int GP; //games played
 	unsigned short int goals;
@@ -14,10 +16,16 @@ struct Players {
 	unsigned short int hits; 
 	unsigned short int blocked_shots; 
 	float faceoff_percent; 
-};
+	//calculated advanced metrics
+	float shot_percent;
+	
+} thePlayers, *PlayerStats;
 
-int readFile (struct Players* allPlayers, const char* tfile);
+int readFile (const char* tfile);
+bool calcStats (int PlayerNumber);
+bool exportData (int Players);
 
+PlayerStats allPlayers;
 int main () {
 	printf ("Welcome to Hockey Manager,\nThis program that can be used to filter and sort statistics provided by you.\
 	\nYour text file will be read and presented back in a comma splicted file (.csv) for you to analyze\n");
@@ -36,22 +44,32 @@ int main () {
 		return -1;
 	}
 	
-	struct Players* People;
 	int people_number = 0; //the number of players that are present in the text file to be analyzed
 	
-	people_number = readFile (People, inputFile);
+	people_number = readFile (inputFile);
 	if (people_number < 0) {
 		printf ("An error occurred when reading data, ensure text file has statistics of each person on a separate line\
-		\nAlso ensure that each statisitc is in order and it is separated by a space(s) or comma(s)\n");
+		\nAlso ensure that each statistic is in order and it is separated by a space(s) or comma(s)\n");
 		return -1;
 	} else if (people_number == 0) {
 		printf ("No data was found in the text file");
 	}
-
+	
+	if (!(calcStats (people_number))) {
+		printf ("\nAn error occurred during advanced metrics calculations");
+		return -1;
+	}
+	
+	if (!(exportData(people_number))) {
+		printf ("An error occurred during exporting data");
+		return -1;
+	}
+	
+	printf ("Test: Program finished running");
 	
 }
 
-int readFile (struct Players* allPlayers, const char* tfile) {
+int readFile (const char* tfile) {
 	int numPpl = 0;
 	FILE* FileIN;
 	
@@ -60,12 +78,9 @@ int readFile (struct Players* allPlayers, const char* tfile) {
 		printf ("Input file could not be opened\n");
 		return -1;
 	}
-	
-	FILE* csvFile;
-	csvFile = fopen("HockeyStatistics.csv", "w'");
-	
+		
 	char temp[500]; int linecheck;
-	while (fgets(temp, 60, FileIN) != NULL) {
+	while (fgets(temp, 499, FileIN) != NULL) { //take in line by line
 		linecheck = 0;
 		while (temp[linecheck] != '\n')
 			linecheck++;
@@ -74,35 +89,62 @@ int readFile (struct Players* allPlayers, const char* tfile) {
 		numPpl++; //count up the number of players to have their statistics inputted
 	}
 	
-	clearerr(FileIN);
+	clearerr(FileIN); //move back to the beginning to now start inputting tne data
 	fseek(FileIN, 0, SEEK_SET);
 	
-	allPlayers = (struct Players*) malloc (numPpl*sizeof(struct Players)); //create appropriate number of structs
+	allPlayers = (PlayerStats) malloc (numPpl*sizeof(thePlayers)); //create appropriate number of structs
 	
 	int i = 0;
 	for ( ; i < numPpl; i++) {
-		fscanf(FileIN, "%49s", allPlayers[i].name);
+		fscanf(FileIN, "%49s", allPlayers[i].name); //take in the first word (should be a name)
+		char temporary[25];
+		fscanf(FileIN, "%24s", temporary); //take in the next word assuming it is the last name
+		
+		//if the first letter is not a character, it means that 2nd word was indeed a statistic, player's name is only 1 word
+		if (temporary[0] > 47 && temporary[0] < 58) { 
+			int ch = atoi (temporary); //convert number to integer
+			int digits = 0;
+			while (ch != 0) { //find the number of digits in the number
+				ch = ch/10;
+				digits++;
+			}
+			fseek (FileIN, -(digits+1), SEEK_CUR); //move back specific number of places according the number of digits
+		} else {
+			strcat(allPlayers[i].name, " "); //if 2nd word was indeed a character, player has a 2 word name
+			strcat(allPlayers[i].name, temporary); // join the 2 words to form one string in the struct
+		}
 		
 		int num_signed_int = 0;
-		int temp[9] = {0};
+		int temp[9] = {0}; //temp array to store data from file
 		
 		for (int structElements = 0; structElements < 9; structElements++) {	
 			
 			if (fscanf (FileIN, "%d", &temp[structElements]) == 0) 
 				continue;
 			
-			if ((structElements < 4 || structElements > 4) && structElements < 8) {
+			if ((structElements < 4 || structElements > 4) && structElements < 10) { //error checking for statistic 1-4 and 5-10
 				if (temp[structElements] < 0 && temp[structElements] > 65535) {
 					printf ("Invalid numbers present in the data provided for player %d, statistics %d", i, structElements);
 					return -1;
 				}
 			}
-				
+			
+			char separation = fgetc (FileIN); //temp char to filter out separating characters between stat numbers
+			while ((separation < '0' || separation > '9') && (separation != '\n' && separation != EOF))
+				separation = fgetc (FileIN);
+			
+			if ((separation == EOF) || separation == '\n') //finish taking in input if EOF or end of line seen
+				continue;
+			else 
+				fseek(FileIN, -1, SEEK_CUR); //move back one character to continue taking in statistics
+		}
+		allPlayers[i].faceoff_percent = 0; 
+		fscanf (FileIN, "%f", &allPlayers[i].faceoff_percent); //directly put in the faceoff_percent value
+		if (allPlayers[i].faceoff_percent < 0) {
+			printf ("Invalid Faceoff Percentage statistic entered, must be a positive value\n");
 		}
 		
-		printf ("%d %d %d %d %d %d %d %d %d", temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7], temp[8]);
-		
-		allPlayers[i].GP = temp[0];
+		allPlayers[i].GP = temp[0]; //transfer over data from temp integer array to struct data
 		allPlayers[i].goals = temp[1];
 		allPlayers[i].assists = temp[2];
 		allPlayers[i].points = temp[3];
@@ -112,19 +154,19 @@ int readFile (struct Players* allPlayers, const char* tfile) {
 		allPlayers[i].hits = temp[7];
 		allPlayers[i].blocked_shots = temp[8];
 		
-			
-		char temp2[500];
-		if (i != 2 && fgets(temp2, 499, FileIN) == NULL) {; //flush out any short remaining text
-			printf ("\nError, more content on a line that needed, check line spaces   %d\n", i);
-			return -1;
-		}
+		fseek (FileIN, -1, SEEK_CUR);
+		char tem = fgetc (FileIN);
+		while (tem != '\n' && tem != EOF) //move past any unneccessary characaters at the end of the line
+			tem = fgetc (FileIN);
+		
 	}
-	
-	for (int c = 0; c < numPpl; c++)
-		printf ("\nName:%s GP:%d Goals:%d Assists: %d Points: %d\n", allPlayers[c].name, allPlayers[c].GP, allPlayers[c].goals, allPlayers[c].assists, allPlayers[c].points);
-	
 	fclose(FileIN); //close the files
-	fclose(csvFile);
 	
-	return numPpl; //return teh value of the number of people;
+	return numPpl; //return the value of the number of people;
 } 
+
+bool calcStats (int PlayerNumber) {
+	int y= 1+1;
+	
+	return true;
+}
