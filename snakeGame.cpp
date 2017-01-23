72fc320573aecc4ee70a20d29206b6f535696a86
ShivamDh <shdh8888@yahoo.ca>
2017-01-22 22:17:34 -0500

    Implementation file of game

---
+++ snakeGame.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include "snakeGame.h"
+using namespace std;
+
+
+
+int Snake::getScore() {
+	return score;
+}
+
+void Snake:: initialGame() {
+	
+}
+
+void Snake::drawCanvas(){
+	
+}
+
+void Snake::keyInput () {
+	
+}
+
+void Snake::gameWork () {
+	
+}
+
+void gamePlay() {
+	
+}
+
+Snake::Snake() {
+	
+}
+
+Snake::Snake(int requiredWidth, int requiredHeight) {
+	
+}
+
+Snake::~Snake {
+	
+}
