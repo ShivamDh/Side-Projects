c3e207633806da25884b564026d547ee415cb88b
ShivamDh <shdh8888@yahoo.ca>
2017-01-24 22:28:38 -0500

    Creating source code file for game

--- snakeGame.cpp
+++ snakeGame.cpp
@@ -12,11 +12,60 @@ int Snake::getScore() {
 }
 
 void Snake:: initialGame() {
+	//start tje game at halt posiiton
+			isGameRunning = true;
+			dir = STOP;
 	
+			//initial snake coordinates
+			xCoor = gameWidth / 4;
+			yCoor = gameHeight / 4;
+			
+			//initial fruit coordinates
+			srand(time(NULL)); //use seeds to ensure that truly random values are generated
+			levelUpx = rand() % gameWidth;
+			levelUpy = rand() % gameHeight;
+			
+			score = 0;
 }
 
 void Snake::drawCanvas(){
+	cout << "  ";
+	for (int i = 0; i < gameWidth+1; i++) {
+		cout << "--";
+	}
 	
+	cout << "-" << endl;
+	
+	for (int k = 0; k < gameHeight; k++) {
+		for (int l = 0; l < 2*gameWidth; l++) {
+			if (l == 0 || l == 2*gameWidth-1)
+				cout << "  |";
+			
+			if (k == yCoor && l == xCoor)
+				cout << "O";
+			else if (l == levelUpx && k == levelUpy)
+				cout << "F";
+			else {
+				bool printedTail = false;
+				for (int b = 0; b < tailLength; b++) {
+					if (tailCoordinates[0][b] == l && tailCoordinates[1][b] == k) {
+						cout << "o";
+						printedTail = true;
+					}
+				}
+				if (!printedTail)
+					cout << " ";
+			}
+		}
+		cout << endl;
+	}
+	cout << "  ";
+	for (int j = 0; j < gameWidth+1; j++) {
+		cout << "--";
+	}
+	cout << "-" << endl;
+	
+	cout << "Score: " << score;
 }
 
 void Snake::keyInput () {
@@ -31,8 +80,7 @@ void gamePlay() {
 	
 }
 
-Snake::Snake() {
-	
+Snake::Snake () { //default constructor
 }
 
 Snake::Snake(int requiredWidth, int requiredHeight) {
