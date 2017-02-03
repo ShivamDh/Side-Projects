ff4945be7b8211994fbb4f3c7908ab5e5766eabe
ShivamDh <shdh8888@yahoo.ca>
2017-02-02 23:32:17 -0500

    Updated header file with code implmentation comments

--- snakeGame.h
+++ snakeGame.h
@@ -10,12 +10,16 @@ class Snake {
 	public:
 		Snake (); //default constructor
 		//constructor for specified user dimensioss for game play
-		Snake (const int requiredWidth, const int requiredHeight);
+		Snake (const int requiredWidth, const int requiredHeight, bool warp = true);
+		// constructor for specified dimensions and chosen symbols for snake head and tail
+		Snake(const int requiredWidth, const int requiredHeight, const char specifiedHead, const char specifiedTail, bool warp = true);
 		~Snake(); //destructor
+		void edit(); //edit function to change gameplay settings
 	
 	private:
 		//paramters
 		bool isGameRunning; //boolean to check is the game still continuing
+		bool warpWalls; //boolean to see if the walls should be warped behind each other (go. go from left end of wall to right end of wall)
 		int gameWidth; //game area width`
 		int gameHeight; //game area height
 		snakeDirection direction; //direction of the snake, defined using enums
@@ -23,6 +27,8 @@ class Snake {
 		int levelUpx, levelUpy; //x and y coordinates of the level up food for snake
 		int score; //current game score
 		int tailLength; //length of the tail
+		char snakeHead; //symbol to represent the head
+		char snakeTail; //symbol to represent the tail
 		//create deque linked lists to store previous coordinates of tail coordinates
 		std::deque <int> tailCoordinates[2]; 
 		void initialGame(); //method for starting the game up 
